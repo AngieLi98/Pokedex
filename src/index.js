3830c1af840c2712b3c748627d9aa589fd002170
Angie Liceth <liceth5262@gmail.com>
2024-07-19 18:21:53 -0500

    se instalo npm de imagenes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import "./style.css";
+import myImage from "./images/tarjeta.png"
 
 const saludar = () => {
     console.log("Hola Mundo!");  
 }
  
 saludar();
+
+const img = document.createElement("img");
+img.src = myImage;
+document.body.appendChild(img);
